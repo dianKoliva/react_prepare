f350ee5959cc1cc43ab517f859b9aa9829e98674
dianKoliva <dianpretty01@gmail.com>
2022-05-12 15:05:16 +0200

    new changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,19 @@ const Contact = () => {
     <div>
 
         <Head></Head>
+
+        <div className="flex">
+            <p>CONTACT US</p>
+
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+laboris nisi ut aliquip ex ea commodo consequat. 
+            </p>
+        </div>
     </div>
   )
 }
