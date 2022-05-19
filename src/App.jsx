8fb1e08e0097921b1f9203fabd6b677339d013a9
dianKoliva <dianpretty01@gmail.com>
2022-05-19 11:03:07 +0200

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,32 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Page from './components/Page';
 import Register from './components/Register';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'   
 
 
 function App() {
   return (
-<Page></Page>
+    <Router>
+     
+    <Routes>
+        {/* <Route path="/" element={<Home title={'Home page'} />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Register />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/in" element={<Page />} />
+
+
+        {/* <Route path="/reset" element={<Recovery title={'Password reset'}  />} />
+        <Route path="/users" element={<UsersList  />} />
+        <Route path="/roles-privs" element={<RolesPriv  />} />
+        <Route path="/items" element={<Items/>}/>
+        <Route path="/recovery" element={<Ps_recovery/>}/>
+        <Route path="/signup" element={<Signup/>}/> */}
+        {/* <Route path="*" element={<NotFound />} /> */}
+    </Routes>
+  </Router>
   )
 }
 
